d639a2bf7e0f61d1f0a0c1ceba85d408e188252e
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2015-05-10 18:26:30 +0200

    LE=[BAS] use a getter Method to get the value of the field

--- ESA/org.dieschnittstelle.jee.esa.basics/src/org/dieschnittstelle/jee/esa/basics/annotations/DisplayAs.java
+++ ESA/org.dieschnittstelle.jee.esa.basics/src/org/dieschnittstelle/jee/esa/basics/annotations/DisplayAs.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD,ElementType.FIELD})
+@Target({ElementType.FIELD})
 public @interface DisplayAs {
 	String value() default "[no value]"; 
 }
