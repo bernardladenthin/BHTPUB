0abfad5bce51252af09d5059aa51f979fb93b5de
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2015-05-18 23:28:51 +0200

    LE=[JRS] JRS2: implement ProductCRUDWebServiceImpl

--- ESA/org.dieschnittstelle.jee.esa.jaxrs/src/org/dieschnittstelle/jee/esa/jaxrs/IProductCRUDWebService.java
+++ ESA/org.dieschnittstelle.jee.esa.jaxrs/src/org/dieschnittstelle/jee/esa/jaxrs/IProductCRUDWebService.java
@@ -3,11 +3,15 @@ package org.dieschnittstelle.jee.esa.jaxrs;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.dieschnittstelle.jee.esa.erp.entities.AbstractProduct;
 import org.dieschnittstelle.jee.esa.erp.entities.IndividualisedProductItem;
 
@@ -25,17 +29,28 @@ import org.dieschnittstelle.jee.esa.erp.entities.IndividualisedProductItem;
 /*
  * UE JRS3: aendern Sie Argument- und Rueckgabetypen der Methoden von IndividualisedProductItem auf AbstractProduct
  */
+@Path("/resteasy/touchpoints")
+@Consumes({ "application/json" })
+@Produces({ "application/json" })
 public interface IProductCRUDWebService {
 
+	@POST
 	public IndividualisedProductItem createProduct(IndividualisedProductItem prod);
 
+	@GET
 	public List<IndividualisedProductItem> readAllProducts();
 
-	public IndividualisedProductItem updateProduct(int id,
+	@PUT
+	@Path("/{productId}")
+	public IndividualisedProductItem updateProduct(@PathParam("productId") int id,
 			IndividualisedProductItem update);
 
-	boolean deleteProduct(int id);
+	@DELETE
+	@Path("/{productId}")
+	boolean deleteProduct(@PathParam("productId") int id);
 
-	public IndividualisedProductItem readProduct(int id);
+	@GET
+	@Path("/{productId}")
+	public IndividualisedProductItem readProduct(@PathParam("productId") int id);
 			
 }
--- ESA/org.dieschnittstelle.jee.esa.jaxrs/src/org/dieschnittstelle/jee/esa/jaxrs/ProductCRUDWebServiceImpl.java
+++ ESA/org.dieschnittstelle.jee.esa.jaxrs/src/org/dieschnittstelle/jee/esa/jaxrs/ProductCRUDWebServiceImpl.java
@@ -1,15 +1,14 @@
 package org.dieschnittstelle.jee.esa.jaxrs;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Context;
 
+import org.apache.log4j.Logger;
 import org.dieschnittstelle.jee.esa.entities.GenericCRUDExecutor;
-import org.dieschnittstelle.jee.esa.erp.entities.AbstractProduct;
 import org.dieschnittstelle.jee.esa.erp.entities.IndividualisedProductItem;
-import org.dieschnittstelle.jee.esa.erp.entities.ProductType;
 
 /*
 UE JRS2: implementieren Sie hier die im Interface deklarierten Methoden
@@ -17,36 +16,44 @@ UE JRS2: implementieren Sie hier die im Interface deklarierten Methoden
 
 public class ProductCRUDWebServiceImpl implements IProductCRUDWebService {
 
+	protected static Logger logger = Logger.getLogger(ProductCRUDWebServiceImpl.class);
+	
+	/**
+	 * this accessor will be provided by the ServletContext, to which it is written by the ProductServletContextListener
+	 */
+	private GenericCRUDExecutor<IndividualisedProductItem> productCRUD;
+	
+	public ProductCRUDWebServiceImpl(@Context ServletContext servletContext, @Context HttpServletRequest request) {
+		logger.info("<constructor>: " + servletContext + "/" + request);
+		// read out the dataAccessor
+		this.productCRUD = (GenericCRUDExecutor<IndividualisedProductItem>)servletContext.getAttribute("productCRUD");
+		
+		logger.debug("read out the productCRUD from the servlet context: " + this.productCRUD);		
+	}
+	
 	@Override
-	public IndividualisedProductItem createProduct(
-			IndividualisedProductItem prod) {
-		// TODO Auto-generated method stub
-		return null;
+	public IndividualisedProductItem createProduct(IndividualisedProductItem prod) {
+		return (IndividualisedProductItem)this.productCRUD.createObject(prod);	
 	}
 
 	@Override
 	public List<IndividualisedProductItem> readAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		return (List)this.productCRUD.readAllObjects();
 	}
 
 	@Override
-	public IndividualisedProductItem updateProduct(int id,
-			IndividualisedProductItem update) {
-		// TODO Auto-generated method stub
-		return null;
+	public IndividualisedProductItem updateProduct(int id, IndividualisedProductItem update) {
+		return (IndividualisedProductItem)this.productCRUD.updateObject(update);	
 	}
 
 	@Override
 	public boolean deleteProduct(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.productCRUD.deleteObject(id);
 	}
 
 	@Override
 	public IndividualisedProductItem readProduct(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.productCRUD.readObject(id);
 	}
 	
 }
