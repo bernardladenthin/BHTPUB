f36ef4705809615c2d3fa9902a4a214dc1fe3812
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2015-05-25 15:52:59 +0200

    LE=[JWS] JWS4, JWS5: add wsconsume scripts to generate new srcgen files

---
+++ jwsxsolution/wsconsume jaxws.client.bat
@@ -0,0 +1,3 @@
+cd /d C:\Users\Bernard\Desktop\ESA\wildfly-8.2.0.Final\bin
+wsconsume.bat -k http://localhost:8080/org.dieschnittstelle.jee.esa.jaxws/TouchpointCRUDWebService?wsdl -o C:\Users\Bernard\Desktop\ESA\eclipseWorkspace\BHTPUB\ESA\org.dieschnittstelle.jee.esa.jaxws.client\srcgen
+pause
---
+++ jwsxsolution/wsconsume jaxws5.bat
@@ -0,0 +1,3 @@
+cd /d C:\Users\Bernard\Desktop\ESA\wildfly-8.2.0.Final\bin
+wsconsume.bat -k http://localhost:8080/esa.uebungen.jws4/ProductCRUDWebService?wsdl -o C:\Users\Bernard\Desktop\ESA\eclipseWorkspace\BHTPUB\ESA\esa.uebungen.jws5\srcgen
+pause
